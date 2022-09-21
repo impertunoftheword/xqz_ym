cb2cbff096c1b110887c28390760ecc44db7f8ad
lym <1971262157@qq.com>
2022-09-21 11:39:45 +0800

    创建文件9_21

---
+++ src/main/java/com/example/xqz/XqzApplication.java
@@ -0,0 +1,13 @@
+package com.example.xqz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XqzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XqzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xqz/XqzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xqz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XqzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
